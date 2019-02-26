7d7967b2f8c3418ce66da170c77c217c4fb3783f
simonnormand <snormand27@gmail.com>
2019-02-26 18:50:58 -0500

    Add files via upload

---
+++ tp1/test/testCorrelation/testCorrelation.cpp
@@ -0,0 +1,31 @@
+
+#include "../test/lib/gtest-1.7.0/include/gtest/gtest.h"
+#include "../../src/calculs.h"
+#include <vector>
+#include <float.h>
+#include <math.h>
+
+TEST(correlation_tests, validnumber) {
+    
+    vector<double> dummyData;
+    dummyData.push_back(-DBL_MAX);
+    
+    EXPECT_EQ(-DBL_MAX, calculCorrelation(dummyData,dummyData,1));
+  
+    dummyData.clear();
+    dummyData.push_back(DBL_MAX);
+    
+    EXPECT_EQ(DBL_MAX, calculCorrelation(dummyData,dummyData,1));
+    
+    dummyData.clear();
+}
+
+
+TEST(correlation_tests, invalidnumber) {
+    
+    
+    dummyData.push_back(DBL_MAX*2);
+    
+    EXPECT_EQ(DBL_MAX, calculCorrelation(dummyData,dummyData,1));
+    dummyData.clear();
+}
