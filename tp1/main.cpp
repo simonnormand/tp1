240d6765526db4a3f5dda211d428fafdfa927455
simonnormand <snormand27@gmail.com>
2019-02-26 19:35:49 -0500

    Add files via upload

--- tp1/main.cpp
+++ tp1/main.cpp
@@ -1,56 +1,19 @@
-#include "src/pch.h"
-#include <iostream>
-#include <string>
-#include "src/calculs.h"
-#include <vector>
-#include "src/CSVReader.h"
+/* 
+ * File:   main.cpp
+ * Author: Simon
+ *
+ * Created on 26 février 2019, 19:34
+ */
 
-int main()
-{
-	double moyenne = 0;
-	double variance = 0;
-	double ecartType = 0;
-        double correlation = 0;
+#include <cstdlib>
 
-	string path = "data/data.csv";
-	vector<double> fullData = readCSV(path);
+using namespace std;
 
-	double nbData = fullData[0];
-	vector<double> data;
-        vector<double> data2;
-	for (int i = 1; i <= nbData*2; i++) {
-            if(i % 2 == 1){
-		data.push_back(fullData[i]);
-            }else{
-                data2.push_back(fullData[i]);
-            }
-	}
+/*
+ * 
+ */
+int main(int argc, char** argv) {
 
-	string temp = "Donnée : { ";
-
-	for (int i = 0; i < 10; i++) {
-		temp = temp + to_string(data[i]) + ", ";
-	}
-
-	temp = temp + "}\n";
-
-	cout << temp;
-
-	moyenne = calculMoyenne(data, nbData);
-
-	cout << "Moyenne : " << moyenne << "\n";
-
-	variance = calculVariance(data, moyenne, nbData);
-
-	cout << "Variance : " << variance << "\n";
-
-	ecartType = calculEcartType(variance);
-
-	cout << "Écart type : " << ecartType << "\n";
-        
-        correlation = calculCorrelation(data, data2, nbData);
-
-	cout << "Corrélation : " << correlation << "\n";
-
-	return 0;
+    return 0;
 }
+
